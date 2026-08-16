9db035fabcfff5243ebe889d4e122676e02a5c1b
ahmadalizaq <aowidat3@gmail.com>
2026-08-16 20:09:49 +0300

    Delete supabase-client.js

--- supabase-client.js
+++
@@ -1,6 +0,0 @@
-// ينشئ عميل Supabase الوحيد المستخدم بكل الموقع (db.js و app.js يستخدمونه عن طريق المتغير sb)
-let sb = null;
-
-if(supabaseReady){
-  sb = supabase.createClient(SUPABASE_CONFIG.url, SUPABASE_CONFIG.anonKey);
-}
